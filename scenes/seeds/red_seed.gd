0ab4a9e1e11c0d865cc0242b5bfd1ffd2adae0b3
Kevin Gage <dudeman02379@gmail.com>
2023-08-15 23:00:28 -0400

    cleaned up spawn logic

--- scenes/seeds/red_seed.gd
+++ scenes/seeds/red_seed.gd
@@ -3,13 +3,12 @@ extends Area2D
 var red_pikmin_scene = preload("res://scenes/pikmin/red_pikmin.tscn")
 
 func _on_area_entered(_area):
+	call_deferred("spawn_pikmin")
+	$".".queue_free()
+
+
+func spawn_pikmin():
 	var new_pikmin: CharacterBody2D = red_pikmin_scene.instantiate()
-	
-	var current_position = $".".position
-	var spawn_position = Vector2(current_position.x, current_position.y - 32)
-	
-	new_pikmin.position = spawn_position
+	new_pikmin.global_position = $SpawnPoint.global_position
 	
 	$"..".add_child(new_pikmin)
-
-	$".".queue_free()
