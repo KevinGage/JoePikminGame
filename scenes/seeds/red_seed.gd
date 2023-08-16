c8229cde40f0e966a50851b58e44e3270e912535
Kevin Gage <dudeman02379@gmail.com>
2023-08-15 22:52:33 -0400

    fixed spawn position error

--- scenes/seeds/red_seed.gd
+++ scenes/seeds/red_seed.gd
@@ -2,13 +2,13 @@ extends Area2D
 
 var red_pikmin_scene = preload("res://scenes/pikmin/red_pikmin.tscn")
 
-func _on_area_entered(area):
+func _on_area_entered(_area):
 	var new_pikmin: CharacterBody2D = red_pikmin_scene.instantiate()
 	
 	var current_position = $".".position
-	var spawn_position = Vector2(current_position.x, current_position.y + 32)
+	var spawn_position = Vector2(current_position.x, current_position.y - 32)
 	
-	new_pikmin.position = $".".position
+	new_pikmin.position = spawn_position
 	
 	$"..".add_child(new_pikmin)
 
